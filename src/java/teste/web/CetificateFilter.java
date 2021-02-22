86a8e69753f7e1f865afebeee64a83acfbe46d02
MrSkulls <hugomes1999@gmail.com>
2021-02-22 18:52:36 +0000

    .

--- src/java/teste/web/CetificateFilter.java
+++ src/java/teste/web/CetificateFilter.java
@@ -7,7 +7,19 @@ public class CetificateFilter implements Filter {
 
 
     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
     }
+
+    @Override
+    public void destroy() {
+
+    }
+
+
 }
